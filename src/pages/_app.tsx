2696feb3d4f12e1eebbd3637b232a9e480961c97
Daniela Vornic <daniela.vornic@gmail.com>
2023-11-18 14:30:01 +0200

    Initiate for translations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { NextIntlClientProvider } from "next-intl";
 import { ChakraProvider, baseTheme, extendTheme } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
@@ -7,6 +9,10 @@ import store from "@/store";
 
 import "@fontsource/libre-baskerville";
 import "@fontsource/inter";
+
+import ro from "@/dictionaries/ro.json";
+import ru from "@/dictionaries/ru.json";
+import en from "@/dictionaries/en.json";
 
 const theme = extendTheme({
   fonts: {
@@ -25,13 +31,18 @@ const theme = extendTheme({
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { locale } = useRouter();
+  const messages = locale === "en" ? en : locale === "ru" ? ru : ro;
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <ChakraProvider theme={theme}>
-          <Component {...pageProps} />
-        </ChakraProvider>
-      </Provider>
-    </QueryClientProvider>
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <ChakraProvider theme={theme}>
+            <Component {...pageProps} />
+          </ChakraProvider>
+        </Provider>
+      </QueryClientProvider>
+    </NextIntlClientProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
 import Head from "next/head";
 import { Heading } from "@chakra-ui/react";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  const t = useTranslations();
+
   return (
     <>
       <Head>
         <title>Create Next App</title>
       </Head>
       <main>
-        <Heading>hello</Heading>
+        <Heading>{t("hi")}</Heading>
       </main>
     </>
   );
